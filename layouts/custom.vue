3f75d5cc3ed51fd1561b5e1e99a3912110ce3790
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2024-04-16 07:20:52 +0200

    add default and custom layout

---
+++ layouts/custom.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <NavBar />
+    <div
+      class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-6 sm:px-10 lg:px-16 pb-1 w-3/4"
+    >
+      <slot />
+    </div>
+  </div>
+</template>
--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -4,16 +4,17 @@ const route = useRoute();
 useHead({
   title: `${route.params.name}`,
 });
+
+definePageMeta({
+  layout: "custom",
+});
 </script>
 
 <template>
   <div>
-    <NavBar />
-    <div class="mx-auto mt-4 max-w-7xl px-4 xs:px-6 sm:px-10 lg:px-16 pb-1">
-      <CarDetailsHero />
-      <CarDetailsAttributes />
-      <CarDetailsDescription />
-      <CarDetailsContact />
-    </div>
+    <CarDetailsHero />
+    <CarDetailsAttributes />
+    <CarDetailsDescription />
+    <CarDetailsContact />
   </div>
 </template>
