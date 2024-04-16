0a1afe075b2bf8ca6192ca1f99f4cfd1dde47cb9
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2024-04-16 06:32:35 +0200

    refactor city/car pages

--- components/Car/SearchBar.vue
+++ components/Car/SearchBar.vue
@@ -1,3 +1,15 @@
+<script setup>
+const city = ref("");
+const cityError = ref(false);
+
+const handleSearch = () => {
+  if (!city.value) {
+    return (cityError.value = true);
+  }
+  navigateTo(`/city/${city.value.trim().toLowerCase()}/car`);
+};
+</script>
+
 <template>
   <div
     class="font-serif w-full text-2xl rounded-full bg-white flex justify-between sm:w-[500px] md:w-[600px] lg:w-[800px] first-letter: overflow-hidden drop-shadow-2xl mx-auto"
@@ -5,8 +17,15 @@
     <input
       type="text"
       class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none"
+      :class="cityError ? 'border-red-500 border' : ''"
       placeholder="Suche in deiner Stadt"
+      v-model="city"
     />
-    <button class="bg-sky-500 px-10 text-white">Suchen</button>
+    <button class="bg-sky-500 px-10 text-white" @click="handleSearch()">
+      Suchen
+    </button>
   </div>
+  <p v-if="cityError" class="text-lg mt-2 p-1 text-red-600">
+    Du hast keine Stadt gewählt.
+  </p>
 </template>
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,6 +2,6 @@
   <header
     class="sticky top-0 z-50 justify-between item-center space-x-1 border-b bg-white p-4 shadow-md h-12 flex items-center"
   >
-    <a href="" class="text-3xl font0mono">carzone</a>
+    <NuxtLink to="/" class="text-3xl font0mono">carzone</NuxtLink>
   </header>
 </template>
